52e6b1950f5f100de3af8ee624e4e8034aa968e1
Friedrich Lindenberg <friedrich@pudo.org>
2022-09-07 14:29:02 +0200

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(path, "README.md"), "r") as f:
 
 setup(
     name="articledata",
-    version="0.0.1",
+    version="0.0.2",
     description="Utility library for trading article data.",
     long_description=readme,
     long_description_content_type="text/markdown",
